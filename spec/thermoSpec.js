3c5f23d6ae6b3e0ad15803cdac171e8efe34dd12
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-17 16:22:46 +0100

    full cycle for many features.

---
+++ spec/thermoSpec.js
@@ -0,0 +1,57 @@
+describe("Thermostat", function() {
+
+  var thermostat;
+
+  beforeEach(function(){
+    // ARRANGE
+    thermostat = new Thermostat();
+  });
+
+  describe("temperature", function(){
+    it("starts at 20 degrees", function(){
+      expect(thermostat.temperature).toBe(20);
+    });
+  });
+
+  describe("#up", function() {
+    it("Increases the temperature", function() {
+      thermostat.up();
+      expect(thermostat.temperature).toBe(25);
+    });
+    it("Throws an error if increase makes temp higher than maximum", function(){
+      thermostat.up();
+      expect(function(){thermostat.up();}).toThrow(new Error("Maximum temperature reached."));
+    });
+  });
+
+  describe("#down", function(){
+    it("Decreases the temperature", function(){
+      thermostat.down();
+      expect(thermostat.temperature).toBe(15);
+    });
+    it("Throws an error if decrease makes temp lower than minimum", function(){
+      thermostat.down();
+      thermostat.down();
+      expect(function(){thermostat.down();}).toThrow(new Error("Minimum temperature reached."));
+    });
+  });
+
+  describe("#togglePowerSaver", function(){
+    it("changes the power saver status", function(){
+      thermostat.togglePowerSaver();
+      expect(thermostat.saver).toBe(false);
+    });
+    it("changes the maximum temperature", function(){
+      thermostat.togglePowerSaver();
+      expect(thermostat.max).toBe(32);
+    });
+  });
+
+  describe("#reset", function(){
+    it("sets the temperature to 20", function(){
+      thermostat.reset();
+      expect(thermostat.temperature).toBe(20);
+    });
+  });
+
+});
---
+++ src/thermo.js
@@ -0,0 +1,40 @@
+
+'esconversion'
+
+
+function Thermostat(){
+  const CHANGE = 5;
+  const MIN_TEMP = 10;
+  const DEFAULT_TEMP = 20;
+  const SAVER_MAX_TEMP = 25
+
+  this.temperature = DEFAULT_TEMP;
+  this.change = CHANGE;
+  this.min = MIN_TEMP;
+  this.max = SAVER_MAX_TEMP;
+  this.saver = true;
+}
+
+Thermostat.prototype.up = function(){
+  if(this.temperature + this.change > this.max){
+    throw new Error("Maximum temperature reached.");
+  }
+  this.temperature += this.change;
+};
+
+Thermostat.prototype.down = function(){
+  if(this.temperature - this.change < this.min){
+    throw new Error("Minimum temperature reached.");
+  }
+  this.temperature -= this.change;
+};
+
+Thermostat.prototype.togglePowerSaver = function(){
+  const MAX_TEMP = 32;
+  this.saver = !this.saver;
+  this.max = MAX_TEMP;
+};
+
+Thermostat.prototype.reset = function(){
+  this.temperature = 20;
+};
