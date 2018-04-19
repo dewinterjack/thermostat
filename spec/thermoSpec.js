1e2ca1e3204b5a42aed37c84e078a4eb0caa63ac
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-19 16:34:22 +0100

    Refactoring.

--- spec/thermoSpec.js
+++ spec/thermoSpec.js
@@ -1,71 +1,79 @@
-describe("Thermostat", function() {
+describe("Thermostat", function () {
 
-  var thermostat;
+	var thermostat;
 
-  beforeEach(function(){
-    // ARRANGE
-    thermostat = new Thermostat();
-  });
+	beforeEach(function () {
+		thermostat = new Thermostat();
+	});
 
-  describe("temperature", function(){
-    it("starts at 20 degrees", function(){
-      expect(thermostat.temperature).toBe(20);
-    });
-  });
+	describe("temperature", function () {
+		it("starts at 20 degrees", function () {
+			expect(thermostat.temperature).toBe(20);
+		});
+	});
 
-  describe("#up", function() {
-    it("Increases the temperature", function() {
-      thermostat.up();
-      expect(thermostat.temperature).toBe(25);
-    });
-    it("Throws an error if increase makes temp higher than maximum", function(){
-      thermostat.temperature = thermostat.max;
-      expect(function(){thermostat.up();}).toThrow(new Error("Maximum temperature reached."));
-    });
-  });
+	describe("#up", function () {
+		it("Increases the temperature", function () {
+			thermostat.up();
+			expect(thermostat.temperature).toBe(25);
+		});
 
-  describe("#down", function(){
-    it("Decreases the temperature", function(){
-      thermostat.down();
-      expect(thermostat.temperature).toBe(15);
-    });
-    it("Throws an error if decrease makes temp lower than minimum", function(){
-      thermostat.temperature = thermostat.min;
-      expect(function(){thermostat.down();}).toThrow(new Error("Minimum temperature reached."));
-    });
-  });
+		it("Throws an error if increase makes temp higher than maximum", function () {
+			thermostat.temperature = thermostat.max;
+			expect(function () {
+				thermostat.up();
+			}).toThrow(new Error("Maximum temperature reached."));
+		});
 
-  describe("#togglePowerSaver", function(){
-    it("changes the power saver status", function(){
-      thermostat.togglePowerSaver();
-      expect(thermostat.saver).toBe(false);
-    });
-    it("changes the maximum temperature", function(){
-      thermostat.togglePowerSaver();
-      expect(thermostat.max).toBe(32);
-    });
-  });
+	});
 
-  describe("#reset", function(){
-    it("sets the temperature to 20", function(){
-      thermostat.reset();
-      expect(thermostat.temperature).toBe(20);
-    });
-  });
+	describe("#down", function () {
+		it("Decreases the temperature", function () {
+			thermostat.down();
+			expect(thermostat.temperature).toBe(15);
+		});
 
-  describe("#energyUsage", function(){
-    it("returns: low-usage | when usage is low", function(){
-      thermostat.temperature = thermostat.low - 1;
-      expect(thermostat.energyUsage()).toBe("low-usage");
-    });
-    it("returns: medium-usage | when usage is medium", function(){
-      thermostat.temperature = thermostat.medium - 1;
-      expect(thermostat.energyUsage()).toBe("medium-usage");
-    });
-    it("returns: high-usage | when usage is high", function(){
-      thermostat.temperature = thermostat.medium + 1;
-      expect(thermostat.energyUsage()).toBe("high-usage");
-    });
-  });
+		it("Throws an error if decrease makes temp lower than minimum", function () {
+			thermostat.temperature = thermostat.min;
+			expect(function () {
+				thermostat.down();
+			}).toThrow(new Error("Minimum temperature reached."));
+		});
+	});
 
+	describe("#togglePowerSaver", function () {
+		it("changes the power saver status", function () {
+			thermostat.togglePowerSaver();
+			expect(thermostat.saver).toBe(false);
+		});
+
+		it("changes the maximum temperature", function () {
+			thermostat.togglePowerSaver();
+			expect(thermostat.max).toBe(32);
+		});
+	});
+
+	describe("#reset", function () {
+		it("sets the temperature to 20", function () {
+			thermostat.reset();
+			expect(thermostat.temperature).toBe(20);
+		});
+	});
+
+	describe("#energyUsage", function () {
+		it("returns: low-usage | when usage is low", function () {
+			thermostat.temperature = thermostat.low - 1;
+			expect(thermostat.energyUsage()).toBe("low-usage");
+		});
+
+		it("returns: medium-usage | when usage is medium", function () {
+			thermostat.temperature = thermostat.medium - 1;
+			expect(thermostat.energyUsage()).toBe("medium-usage");
+		});
+
+		it("returns: high-usage | when usage is high", function () {
+			thermostat.temperature = thermostat.medium + 1;
+			expect(thermostat.energyUsage()).toBe("high-usage");
+		});
+	});
 });
