4fc2101f82499387ccfcf7d099d4d25087217ce0
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-19 16:40:27 +0100

    Increase temperature button jQuery.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	var thermostat = new Thermostat();
 	$('#temperature').text(thermostat.temperature);
+	$('#increase_temp').on('click', function(){
+		thermostat.up();
+		$('#temperature').text(thermostat.temperature);
+	});
 });
